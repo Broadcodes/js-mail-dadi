5ca56b72df0bfb9f2c9f80a70cf2b92aa62e9c24
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-08-26 19:16:49 +0200

    Visualizzare risultato casuale a schermo

--- js/script.js
+++ js/script.js
@@ -2,16 +2,6 @@
 const dadoComputer = [1, 2, 3, 4, 5, 6];
 const dadoUtente = dadoComputer;
 
-// Genero dei valori che vanno da 0 a 5 per indicare gli indici dell'array
-let valoreEstrattoComputer = Math.ceil((Math.random() * 10).toFixed() / 2);
-let valoreEstrattoUtente = Math.ceil((Math.random() * 10).toFixed() / 2);
-
-const valoreDadoComputer = dadoComputer[valoreEstrattoComputer];
-const valoreDadoUtente = dadoUtente[valoreEstrattoUtente];
-
-console.log(`Computer: ${valoreDadoComputer}`);
-console.log(`Utente: ${valoreDadoUtente}`);
-
 // Modifico il colore del pulsante se il mouse va sopra l'elemento
 const pulsanteGioca = document.getElementById("lanciaDado");
 pulsanteGioca.addEventListener("mouseenter", function(){
@@ -24,6 +14,15 @@ pulsanteGioca.addEventListener("mouseleave", function(){
 
 // Se viene cliccato il pulsante genere l'evento ovvero mostra il risultato del gioco
 pulsanteGioca.addEventListener("click", function(){
+// Genero dei valori che vanno da 0 a 5 per indicare gli indici dell'array
+let valoreEstrattoComputer = Math.ceil((Math.random() * 10).toFixed() / 2);
+let valoreEstrattoUtente = Math.ceil((Math.random() * 10).toFixed() / 2);
+
+const valoreDadoComputer = dadoComputer[valoreEstrattoComputer];
+const valoreDadoUtente = dadoUtente[valoreEstrattoUtente];
+
+console.log(`Computer: ${valoreDadoComputer}`);
+console.log(`Utente: ${valoreDadoUtente}`);
 
 // Mostra a schermo se il vincitore è il computer, l'utente o invece è pareggio
 

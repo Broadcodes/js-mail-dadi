5c8b313941675e4293e5a3b3d0daa18f2dc4ec7c
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-08-26 16:12:07 +0200

    Generazione random valori da 0 a 5 per gli indici

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 // Creo due array che contengono i 6 valori dei dadi
-const computer = [1, 2, 3, 4, 5, 6];
-const utente = computer;
+const dadoComputer = [1, 2, 3, 4, 5, 6];
+const dadoUtente = dadoComputer;
 
-console.log(computer);
-console.log(utente);
+// Genero dei valori che vanno da 0 a 5 per indicare gli indici dell'array
+let valoreEstrattoComputer = Math.ceil((Math.random() * 10).toFixed() / 2);
+let valoreEstrattoUtente = Math.ceil((Math.random() * 10).toFixed() / 2);
+
+const valoreDadoComputer = dadoComputer[valoreEstrattoComputer];
+const valoreDadoUtente = dadoUtente[valoreEstrattoUtente];
+
+console.log(`Computer: ${valoreDadoComputer}`);
+console.log(`Utente: ${valoreDadoUtente}`);
+
 
